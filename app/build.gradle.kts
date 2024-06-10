3ee5026ae5bfef7bbb6187a493f37f953e1f097d
Andres Lopez <alopez@booz.cl>
2024-06-10 15:42:09 -0500

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -33,6 +35,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +50,18 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Hilt
+    implementation(libs.hiltAndroid)
+    kapt(libs.hiltCompiler)
+
+    // ViewModel
+    implementation(libs.viewmodel)
+
+
+}
+
+
+kapt {
+    correctErrorTypes = true
 }
